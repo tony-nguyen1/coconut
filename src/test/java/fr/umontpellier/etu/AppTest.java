f7c700ce819a7d7c4760ddbc4f64d624a475034b
tony-nguyen1 <nguyentoony@gmail.com>
2024-02-12 17:19:37 +0100

    first commit

---
+++ src/test/java/fr/umontpellier/etu/AppTest.java
@@ -0,0 +1,18 @@
+package fr.umontpellier.etu;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
